0d67616d7c3f15046d18ae268227aaab53f189b7
Pablo O.Mesquita <pablo_omesquita@hotmail.com>
2023-08-11 22:45:51 -0300

    ajuste pagina de notfound

--- api/index.php
+++ api/index.php
@@ -5,8 +5,27 @@ use Source\App\HomeController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Container;
 
-$app = new App();
+$c = new Container(); //Create Your container
+//Override the default Not Found Handler before creating App
+$c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        echo json_encode([
+            "errors" => [
+                "type " => "endpoint_not_found",
+                "message" => "Não foi possível processar a requisição",
+                "status" => 404
+            ]
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+        return $response->withStatus(404)
+                        ->withHeader('Content-Type', 'application/json');
+    };
+};
+
+$app = new App($c);
+
 
 ### USERS ###
 //user
@@ -23,4 +42,3 @@ $app->get("/post", "Source\App\Api\ApiPostController:getPosts");
 $app->get("/post/{id}", "Source\App\Api\ApiPostController:getPost");
 
 $app->run();
-
